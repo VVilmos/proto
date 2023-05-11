3518be9c63c89f4a702566ce3efd139a6dc12f11
Kiss Blanka Zselyke <kissblanka02@gmail.com>
2023-05-11 12:19:27 +0200

    parancsok blanka

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -1,6 +1,7 @@
 package Model;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Game {
     /**
@@ -49,4 +50,70 @@ public class Game {
     public static Pool getMechanicPool() {
         return mechanicPool;
     }
+
+    //ide jöhetnek a parancsok
+
+    public void Add(String type, String name){
+        if(Objects.equals(type, "Pipe")){
+            Pipe p = new Pipe(null);
+            pipes.put(name, p);
+        }
+        if(Objects.equals(type, "Pump")){
+            Pump p = new Pump();
+            pumps.put(name, p);
+        }
+        if(Objects.equals(type, "Cistern")){
+            Cistern c = new Cistern();
+            cisterns.put(name, c);
+        }
+        if(Objects.equals(type, "Source")){
+            Source s = new Source();
+            sources.put(name, s);
+        }
+        if(Objects.equals(type, "Mechanic")){
+            Mechanic m = new Mechanic();
+            mechanics.put(name, m);
+        }
+        if(Objects.equals(type, "Saboteur")){
+            Saboteur s = new Saboteur();
+            saboteurs.put(name, s);
+        }
+    }
+
+    public void Drain(String name){
+        if(pipes.containsKey(name)){
+            pipes.get(name).RemoveWater();
+        }
+        else if(pumps.containsKey(name)){
+            pumps.get(name).EmptyWaterTank();
+        }
+    }
+
+    public void HoldPipe(String pipename, String mechanicname){
+        if(pipes.containsKey(pipename) && mechanics.containsKey(mechanicname)){
+            mechanics.get(mechanicname).HoldPipe(pipes.get(pipename));
+        }
+    }
+
+    public void HoldPump(String pumpname, String mechanicname){
+        if(pumps.containsKey(pumpname) && mechanics.containsKey(mechanicname)){
+            mechanics.get(mechanicname).HoldPump(pumps.get(pumpname));
+        }
+    }
+
+    public void Step(String name){
+        if(pipes.containsKey(name)){
+            pipes.get(name).Step();
+        }
+        else if(pumps.containsKey(name)){
+            pumps.get(name).Step();
+        }
+        else if(cisterns.containsKey(name)){
+            cisterns.get(name).Step();
+        }
+        else if(sources.containsKey(name)){
+            sources.get(name).Step();
+        }
+    }
+
 }
