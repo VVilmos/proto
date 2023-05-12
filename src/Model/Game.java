cedd46a6afb004b967b4722c2394dfdd437150f5
Vilmos Vörös <vvoros@edu.bme.hu>
2023-05-12 12:00:43 +0200

    Vili mod -2 könnyebb kiiratas

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -14,11 +14,7 @@ public class Game {
     private HashMap<String, Source> sources = new HashMap<>();
     private HashMap<String, Mechanic> mechanics = new HashMap<>();
     private HashMap<String, Saboteur> saboteurs = new HashMap<>();
-    private HashMap<String, Element> elements = new HashMap<>();
-
-    private HashMap<String, Node> nodes = new HashMap<>();
-
-    private HashMap<String, Player> players = new HashMap<>();
+   private HashMap<Object, String> objectnames = new HashMap<>();
 
     /**
      * A játék determinisztikusságát határozza meg.
@@ -177,13 +173,8 @@ public class Game {
             Mechanic m = mechanics.get(objectname);
             Element on = m.GetLocation();                                               //nem létező függvény!!!
 
-            System.out.println("On: ");
+            System.out.println("On: " + objectnames.get(on));
 
-            //kulcs keresése az érték alapján --> tartózkodási hely keresése
-            for (Map.Entry<String, Element> entry: elements.entrySet()) {
-                if (entry.getValue().equals(on)) System.out.println(entry.getKey());
-                break;
-            }
 
             System.out.print("holdingPipe: ");
 
@@ -192,24 +183,15 @@ public class Game {
                 return;
             }
 
-            //kulcs keresése az érték alapján --> kezében lévő cső keresése
+            //ha van a kezében cső
             Pipe holdpipe = m.holdingPipeEnd.GetOwnPipe();                               //elérhetem a holdingPipeEndet?????
-            for (Map.Entry<String, Pipe> entry: pipes.entrySet()) {
-                if (entry.getValue().equals(holdpipe)) System.out.println(entry.getKey());
-                break;
-            }
+            System.out.println(objectnames.get(holdpipe));
         }
         else if (saboteurs.containsKey(objectname)) {
             Saboteur s = saboteurs.get(objectname);
             Element on = s.GetLocation();                                               //nem létező függvény!!!
 
-            System.out.println("On: ");
-
-            //kulcs keresése az érték alapján --> tartózkodási hely keresése
-            for (Map.Entry<String, Element> entry: elements.entrySet()) {
-                if (entry.getValue().equals(on)) System.out.println(entry.getKey());
-                break;
-            }
+            System.out.println("On: " + objectnames.get(on));
 
             System.out.print("holdingPipe: ");
 
@@ -218,34 +200,26 @@ public class Game {
                 return;
             }
 
-            //kulcs keresése az érték alapján --> kezében lévő cső keresése
+            //ha van a kezében cső
             Pipe holdpipe = s.holdingPipeEnd.GetOwnPipe();                               //elérhetem a holdingPipeEndet?????
-            for (Map.Entry<String, Pipe> entry: pipes.entrySet()) {
-                if (entry.getValue().equals(holdpipe)) System.out.println(entry.getKey());
-                break;
-            }
+            System.out.println(objectnames.get(holdpipe));
         }
         else if (pipes.containsKey(objectname)) {
             Pipe pip = pipes.get(objectname);
             if (args.equals("")) {
+
+                //helybéli karakter(ek)
                 var list = pip.GetPlayers();
-                //Megkeressük az értékhez tartozó kulcsot --> helyben lévő karakterek listázása
                 System.out.print("Player: ");
-                for (Map.Entry<String, Player> entry: players.entrySet()) {
-                    for (Player p : list) {
-                        if (entry.getValue().equals(p)) System.out.print(entry.getKey() + " ");
-                    }
-                }
+                for (Player p : list) System.out.print(objectnames.get(p) + " ");
                 System.out.println();
+
+                //cső végei
                 var list2 = pip.GetNeighbours();
-                System.out.print("End1: ");
-                for (Map.Entry<String, Element> entry: elements.entrySet()) {
-                    if (entry.getValue().equals(list2.get(0))) System.out.print(entry.getKey() + "\n");
-                }
-                System.out.print("End2: ");
-                for (Map.Entry<String, Element> entry: elements.entrySet()) {
-                    if (entry.getValue().equals(list2.get(1))) System.out.print(entry.getKey() + "\n");
-                }
+                System.out.println("End1: " + objectnames.get(list2.get(0)));
+                System.out.println("End2: " + objectnames.get(list2.get(1)));
+
+                //állapotok
                 System.out.print("isBroken: ");
                 if (pip.GetBrokennes()) System.out.println("true");
                 else System.out.println("false");
@@ -264,30 +238,19 @@ public class Game {
 
                 System.out.println();
 
-            }
-            else {
+            } else {
                 if (args.contains("o")) {
                     var list = pip.GetPlayers();
-
-                    //Megkeressük az értékhez tartozó kulcsot --> helyben lévő karakterek listázása
                     System.out.print("Player: ");
-                    for (Map.Entry<String, Player> entry: players.entrySet()) {
-                        for (Player p : list) {
-                            if (entry.getValue().equals(p)) System.out.print(entry.getKey() + " ");
-                        }
-                    }
+                    for (Player p : list) System.out.print(objectnames.get(p) + " ");
                     System.out.println();
+
                 }
                 if (args.contains("e")) {
-                    var list = pip.GetNeighbours();
-                    System.out.print("End1: ");
-                    for (Map.Entry<String, Element> entry: elements.entrySet()) {
-                        if (entry.getValue().equals(list.get(0))) System.out.print(entry.getKey() + "\n");
-                    }
-                    System.out.print("End2: ");
-                    for (Map.Entry<String, Element> entry: elements.entrySet()) {
-                        if (entry.getValue().equals(list.get(1))) System.out.print(entry.getKey() + "\n");
-                    }
+                    var list2 = pip.GetNeighbours();
+                    System.out.println("End1: " + objectnames.get(list2.get(0)));
+                    System.out.println("End2: " + objectnames.get(list2.get(1)));
+
                 }
                 if (args.contains("b")) {
                     System.out.print("isBroken: ");
@@ -317,62 +280,50 @@ public class Game {
 
             }
             System.out.println();
-
         }
         else if (pumps.containsKey(objectname)) {
             Pump pu = pumps.get(objectname);
             if (args.equals("")) {
-                var list = pu.GetPlayers();
 
-                //játékosok
-                //Megkeressük az értékhez tartozó kulcsot --> helyben lévő karakterek listázása
+                //helybéli karakterek
+                var list = pu.GetPlayers();
                 System.out.print("Player: ");
-                for (Map.Entry<String, Player> entry: players.entrySet()) {
-                    for (Player p : list) {
-                        if (entry.getValue().equals(p)) System.out.print(entry.getKey() + " ");
-                    }
+                for (Player p : list) {
+                    System.out.print(objectnames.get(p) +" ");
                 }
                 System.out.println();
 
                 //csatlakoztatott csövek
-                //Értékhez kulcs keresése a hashmapben --> Csatlakoztatott csövek listázása
-                System.out.print("\nConnectedPipes: ");
-                PipeEnd[] ends = pu.GetPipeEnds();
-                for (Map.Entry<String, Pipe> entry: pipes.entrySet()) {
-                    for (PipeEnd pe : ends) {
-                        if (entry.getValue().equals(pe.GetOwnPipe())) System.out.print(entry.getKey() + " ");
-                    }
+                System.out.print("ConnectedPipes: ");
+                var neighbours = pu.GetNeighbours();,
+                for (Element neighbour: neighbours) {
+                    System.out.print(objectnames.get(neighbour));
                 }
+                System.out.println();
+
                 //Bemeneti cső
                 System.out.print("inPipe: ");
-                if (pu.GetInPipeNumber() == -1) System.out.print("null\n");
+                if (pu.GetInPipeNumber() == -1) System.out.println("null");
                 else {
+                    PipeEnd[] ends = pu.GetPipeEnds();
                     for (int i = 0; i < ends.length; i++) {
                         if (i == pu.GetInPipeNumber()) {
                             Pipe pip = ends[i].GetOwnPipe();
-                            for (Map.Entry<String, Pipe> entry: pipes.entrySet()) {
-                                if (entry.getValue().equals(pip)) {
-                                    System.out.print(entry.getKey() + "\n");
-                                    break;
-                                }
-                            }
+                            System.out.println(objectnames.get(pip));
                             break;
                         }
                     }
                 }
+
                 //Kimeneti cső
                 System.out.print("outPipe: ");
-                if (pu.GetOutPipeNumber() == -1) System.out.print("null\n");
+                if (pu.GetOutPipeNumber() == -1) System.out.println("null");
                 else {
+                    PipeEnd[] ends = pu.GetPipeEnds();
                     for (int i = 0; i < ends.length; i++) {
                         if (i == pu.GetOutPipeNumber()) {
                             Pipe pip = ends[i].GetOwnPipe();
-                            for (Map.Entry<String, Pipe> entry: pipes.entrySet()) {
-                                if (entry.getValue().equals(pip)) {
-                                    System.out.print(entry.getKey() + "\n");
-                                    break;
-                                }
-                            }
+                            System.out.println(objectnames.get(pip));
                             break;
                         }
                     }
@@ -390,60 +341,47 @@ public class Game {
             }
             else {
                 if (args.contains("o")) {
-                    var list = pu.GetPlayers();
 
-                    //Megkeressük az értékhez tartozó kulcsot --> helyben lévő karakterek listázása
+                    var list = pu.GetPlayers();
                     System.out.print("Player: ");
-                    for (Map.Entry<String, Player> entry: players.entrySet()) {
-                        for (Player p : list) {
-                            if (entry.getValue().equals(p)) System.out.print(entry.getKey() + " ");
-                        }
+                    for (Player p : list) {
+                       System.out.print(objectnames.get(p)+ " ");
                     }
                     System.out.println();
                 }
                 if (args.contains("p")) {
-                    //Értékhez kulcs keresése a hashmapben --> Csatlakoztatott csövek listázása
-                    System.out.print("\nConnectedPipes: ");
+
+                    System.out.print("ConnectedPipes: ");
                     PipeEnd[] ends = pu.GetPipeEnds();
-                    for (Map.Entry<String, Pipe> entry: pipes.entrySet()) {
-                        for (PipeEnd pe : ends) {
-                            if (entry.getValue().equals(pe.GetOwnPipe())) System.out.print(entry.getKey() + " ");
-                        }
+                    for (PipeEnd pe: ends) {
+                        System.out.print(objectnames.get(pe.GetOwnPipe()));
                     }
+
+                    System.out.println();
                 }
                 if (args.contains("f")) {
-                    PipeEnd[] ends = pu.GetPipeEnds();
                     System.out.print("inPipe: ");
-                    if (pu.GetInPipeNumber() == -1) System.out.print("null\n");
+                    if (pu.GetInPipeNumber() == -1) System.out.println("null");
                     else {
+                        PipeEnd[] ends = pu.GetPipeEnds();
                         for (int i = 0; i < ends.length; i++) {
                             if (i == pu.GetInPipeNumber()) {
                                 Pipe pip = ends[i].GetOwnPipe();
-                                for (Map.Entry<String, Pipe> entry: pipes.entrySet()) {
-                                    if (entry.getValue().equals(pip)) {
-                                        System.out.print(entry.getKey() + "\n");
-                                        break;
-                                    }
-                                }
+                                System.out.println(objectnames.get(pip));
                                 break;
                             }
                         }
                     }
                 }
                 if (args.contains("t")) {
-                    PipeEnd[] ends = pu.GetPipeEnds();
                     System.out.print("outPipe: ");
-                    if (pu.GetOutPipeNumber() == -1) System.out.print("null\n");
+                    if (pu.GetOutPipeNumber() == -1) System.out.println("null");
                     else {
+                        PipeEnd[] ends = pu.GetPipeEnds();
                         for (int i = 0; i < ends.length; i++) {
                             if (i == pu.GetOutPipeNumber()) {
                                 Pipe pip = ends[i].GetOwnPipe();
-                                for (Map.Entry<String, Pipe> entry: pipes.entrySet()) {
-                                    if (entry.getValue().equals(pip)) {
-                                        System.out.print(entry.getKey() + "\n");
-                                        break;
-                                    }
-                                }
+                                System.out.println(objectnames.get(pip));
                                 break;
                             }
                         }
@@ -466,43 +404,44 @@ public class Game {
         }
         else if (cisterns.containsKey(objectname)) {
             Cistern cis = cisterns.get(objectname);
-            var list = cis.GetPlayers();
 
-            //Megkeressük az értékhez tartozó kulcsot --> helyben lévő karakterek listázása
-            System.out.print("Players: ");
-            for (Map.Entry<String, Player> entry: players.entrySet()) {
-                for (Player p : list) {
-                    if (entry.getValue().equals(p)) System.out.print(entry.getKey() + " ");
-                }
+            //helybéli karakterek
+            var list = cis.GetPlayers();
+            System.out.print("Player: ");
+            for (Player p : list) {
+                System.out.print(objectnames.get(p)+ " ");
             }
-            //Értékhez kulcs keresése a hashmapben --> Csatlakoztatott csövek listázása
-            System.out.print("\nConnectedPipes: ");
+            System.out.println();
+
+            //csatlkaztatott csövek
+            System.out.print("ConnectedPipes: ");
             PipeEnd[] ends = cis.GetPipeEnds();
-            for (Map.Entry<String, Pipe> entry: pipes.entrySet()) {
-                for (PipeEnd pe : ends) {
-                    if (entry.getValue().equals(pe.GetOwnPipe())) System.out.print(entry.getKey() + " ");
-                }
+            for (PipeEnd pe: ends) {
+                System.out.print(objectnames.get(pe.GetOwnPipe()));
             }
+
+            System.out.println();
 
         }
         else if (sources.containsKey(objectname)) {
             Source source = sources.get(objectname);
+
+            //helybéli karakterek
             var list = source.GetPlayers();
-            //Megkeressük az értékhez tartozó kulcsot --> helyben lévő karakterek listázása
-            System.out.print("Players: ");
-            for (Map.Entry<String, Player> entry: players.entrySet()) {
-                for (Player p : list) {
-                    if (entry.getValue().equals(p)) System.out.print(entry.getKey() + " ");
-                }
+            System.out.print("Player: ");
+            for (Player p : list) {
+                System.out.print(objectnames.get(p)+ " ");
             }
-            //Értékhez kulcs keresése a hashmapben --> Csatlakoztatott csövek listázása
-            System.out.print("\nConnectedPipes: ");
+            System.out.println();
+
+            //csatlkaztatott csövek
+            System.out.print("ConnectedPipes: ");
             PipeEnd[] ends = source.GetPipeEnds();
-            for (Map.Entry<String, Pipe> entry: pipes.entrySet()) {
-                for (PipeEnd pe : ends) {
-                    if (entry.getValue().equals(pe.GetOwnPipe())) System.out.print(entry.getKey() + " ");
-                }
+            for (PipeEnd pe: ends) {
+                System.out.print(objectnames.get(pe.GetOwnPipe()));
             }
+
+            System.out.println();
         }
         else {
             System.out.println("Unknown object! Note that all referred objects are to be added to the running model.");
