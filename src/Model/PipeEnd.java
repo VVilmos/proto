134c4c908f195ff57668e0e9fa13a79071e02868
gergopc <gergoesjanka@gmail.com>
2023-05-12 17:08:07 +0200

    PipeEnd implemented

--- src/Model/PipeEnd.java
+++ src/Model/PipeEnd.java
@@ -14,40 +14,40 @@ public class PipeEnd {
      * Az az aktív elem/csúcs, amelyhez a csővég csatlakoztatva van
      * Ha a csőnek ez a vége szabad, akkor a mező értéke null
      */
-    private Node node;
+    private Node node = null;
 
     /**
      * A PipeEnd konstruktora \n
      * Hozzárendeli, felcsatlakoztatja a paraméterként kapott csőre a csővégt.
+     *
      * @param p A cső, amire kerül ez a csővég.
      */
     public PipeEnd(Pipe p) {
         this.pipe = p;
     }
+
     /**
      * Getter a csővéghez tartozó csőre.
+     *
      * @return Visszatéríti a csövet, aminek ez a csővég a vége.
      */
     public Pipe GetOwnPipe() {
-
         return pipe;
     }
 
     /**
      * Továbbítja a vizet a csőnek.
+     *
      * @return Igazzal tér vissza, ha a cső ({@link Pipe}) képes vizet befogadni.
      */
     public boolean AcceptWater() {
         boolean accepted = pipe.AcceptWater();
-        if(accepted){
-        }
-        else{
-        }
         return accepted;
     }
 
     /**
      * Kiszívja, eltávolítja a vizet a csővéghez tartozó csőből.
+     *
      * @return Igazzal tér vissza, ha a csőből ({@link Pipe}) lehet vizet kiszívni.
      */
     public boolean RemoveWater() {
@@ -57,27 +57,36 @@ public class PipeEnd {
 
     /**
      * A paraméterként kapott {@link Node}-hoz kapcsolja a csővéget.
+     *
      * @param node A {@link Node}, amire a csővéget felcsatlakoztatjuk.
      */
     public void ConnectNode(Node node) {
-
         this.node = node;
     }
 
     /**
      * Lecsatlakoztatja a csővéget a felkapcsolt {@link Node}-ról.
      */
-    public void DisconnectFromNode(){  //pontadas?
+    public void DisconnectFromNode() {  //pontadas?
         boolean isFull = pipe.RemoveWater();
         if (isFull) Game.getSaboteurPool().AddWater();
+        node = null;
     }
 
     /**
      * Getter a {@link Node}-ra, amire a csővég rá van kapcsolva.
+     *
      * @return Visszatéríti a csővéghez kapcsolódó {@link Node}-ot.
      */
-    public Node GetAttachedNode(){
-
+    public Node GetAttachedNode() {
         return this.node;
+    }
+
+    /**
+     * Kiírja a csővég belső állapotát
+     */
+    public void GetState(){
+        System.out.println("pipe:" /* a cső neve*/);
+        System.out.println("node: " /* a node neve*/);
     }
 }
