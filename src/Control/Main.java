79150a00a4b76b4555544c1dc3a8cbdd3bb2f6c3
gergopc <gergoesjanka@gmail.com>
2023-05-29 13:44:33 +0200

    RefreshTimer implemented

---
+++ src/Control/Main.java
@@ -0,0 +1,19 @@
+package Control;
+
+import Model.Game;
+
+import java.util.Scanner;
+
+import Model.Timer;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    private View.RefreshTimer refreshTimer;
+
+    public static void main(String[] args) {
+
+      Main main = new Main();
+    }
+}
