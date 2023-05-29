4812f29357040e83e7858d066084832f18b3216e
Kiss Blanka Zselyke <kissblanka02@gmail.com>
2023-05-29 11:26:38 +0200

    jo

--- src/View/PumpView.java
+++ src/View/PumpView.java
@@ -16,6 +16,10 @@ public class PumpView extends ElementView{
         g2d.setColor(new Color(255, 96, 71));
         Point center = GetCenterCoordinates();
         g2d.fillOval(center.GetX(), center.GetY(), 40, 40);
+        if(pump.GetBrokenness()){
+            g2d.setColor(new Color(0, 0, 0));
+            g2d.fillOval(center.GetX() + 15, center.GetY() + 15, 10, 10);
+        }
     }
 
 }
